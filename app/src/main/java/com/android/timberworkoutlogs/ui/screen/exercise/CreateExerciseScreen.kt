235a62911c82fdc100cf2c5ce2c17a71f5897c1b
Phil Boyd <81324494+BoydyP@users.noreply.github.com>
2025-06-30 20:35:16 +0100

    chore: Clean up lazy * import from CreateExerciseScreen refactor

--- app/src/main/java/com/android/timberworkoutlogs/ui/screen/exercise/CreateExerciseScreen.kt
+++ app/src/main/java/com/android/timberworkoutlogs/ui/screen/exercise/CreateExerciseScreen.kt
@@ -1,6 +1,13 @@
 package com.android.timberworkoutlogs.ui.screen.exercise
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
