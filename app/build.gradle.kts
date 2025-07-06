4ca2a1f8d59283d3063ad09cd911b4c2149fddc4
Phil Boyd <81324494+BoydyP@users.noreply.github.com>
2025-07-06 16:36:08 +0100

    chore: Lift compile version to 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 
 android {
     namespace = "com.android.timberworkoutlogs"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.android.timberworkoutlogs"
