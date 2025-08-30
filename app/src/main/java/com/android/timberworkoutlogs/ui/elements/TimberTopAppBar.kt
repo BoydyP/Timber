b8a43189ea9225c45689885b68bcd30a34b87e6d
Phil Boyd <81324494+BoydyP@users.noreply.github.com>
2025-08-30 19:58:21 +0100

    feat: Add test tag to Timber app logo
    This commit adds a test tag `TimberAppLogo` to the `IconButton` containing the app logo in `TimberTopAppBar.kt`. This allows the logo to be easily identifiable in UI tests.

--- app/src/main/java/com/android/timberworkoutlogs/ui/elements/TimberTopAppBar.kt
+++ app/src/main/java/com/android/timberworkoutlogs/ui/elements/TimberTopAppBar.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.android.timberworkoutlogs.R
@@ -26,7 +27,10 @@ fun TimberTopAppBar(
     TopAppBar(
         title = { Text("Timber", style = MaterialTheme.typography.headlineSmall) },
         actions = {
-            IconButton(onClick = onIconClick) {
+            IconButton(
+                onClick = onIconClick,
+                modifier = Modifier.testTag("TimberAppLogo")
+            ) {
                 Image(
                     painter = painterResource(id = R.drawable.timberlogo),
                     contentDescription = "App Logo",
